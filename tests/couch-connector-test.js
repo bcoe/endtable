08b00afe5c328a0ed2baa24e16fc26c580d9e3c3
bcoe <bencoe@gmail.com>
2011-12-10 14:33:05 -0800

    The original unit testing approach for this library was suspect, I'm updating it to a better unit testing framework.

---
+++ tests/couch-connector-test.js
@@ -0,0 +1,243 @@
+var equal = require('assert').equal,
+    puts = require('sys').puts,
+	connector = require('../lib/couch-connector').connector,
+	endtable = require('../lib');
+
+exports.tests = {
+	'should generate a restful URL based on a document id': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connection = {
+			request: function(url) {
+				lastUrl = url;
+			}
+		}
+
+		c.loadDocument('foobar');
+		equal('/foobar', lastUrl, prefix + ' proper RESTful url not created.');
+		finished();
+	},
+	
+	'should generate a restful URL based on DB and Keys': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connection = {
+			request: function(url) {
+				lastUrl = url;
+			}
+		}
+		c.loadDocument({
+			keys: ['name', 'age'],
+			type: 'person'
+		})
+		equal('/_design/person/_view/by_name_age', lastUrl, prefix + ' proper RESTful url not created.');
+		finished();
+	},
+	
+	'should build a URL with the get parameter descending': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connection = {
+			request: function(url) {
+				lastUrl = url;
+			}
+		}
+		c.loadDocument({
+			keys: ['name', 'age'],
+			type: 'person',
+			descending: true
+		})
+		equal('/_design/person/_view/by_name_age?descending=true', lastUrl, prefix + ' proper RESTful url not created.');
+		finished();
+	},
+	
+	'should build a URL with the get parameters descending and startkey': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connection = {
+			request: function(url) {
+				lastUrl = url;
+			}
+		}
+		c.loadDocument({
+			keys: ['name', 'age'],
+			type: 'person',
+			descending: true,
+			startkey: ['john', '13'],
+			endkey: 'foo'
+		})
+		equal(true, ( lastUrl.indexOf('startkey=%5B%22john%22%2C%2213%22%5D') > 0 ), prefix + ' proper RESTful url not created.');
+		equal(true, ( lastUrl.indexOf('descending=true') > 0 ), prefix + ' proper RESTful url not created.');
+		finished();
+	},
+	
+	'should build a URL with the get parameters startkey_docid and endkey_docid': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connection = {
+			request: function(url) {
+				lastUrl = url;
+			}
+		}
+		c.loadDocument({
+			startkey_docid: "foo",
+			endkey_docid: "bar"
+		})
+		equal(true, ( lastUrl.indexOf('startkey_docid=%22foo%22') > 0 ), prefix + ' proper RESTful url not created.');
+		equal(true, ( lastUrl.indexOf('endkey_docid=%22bar%22') > 0 ), prefix + ' proper RESTful url not created.');
+		finished();
+	},
+	
+	'should build a URL with the get paremeters limit and skip': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connection = {
+			request: function(url) {
+				lastUrl = url;
+			}
+		}
+		c.loadDocument({
+			keys: ['name', 'age'],
+			type: 'person',
+			limit: 1,
+			skip: 1
+		})
+		equal(true, ( lastUrl.indexOf('limit=1') > 0 ), prefix + ' proper RESTful url not created.');
+		equal(true, ( lastUrl.indexOf('skip=1') > 0 ), prefix + ' proper RESTful url not created.');
+		finished();
+	},
+	
+	'should create an exception if the requested view does not exist': function(finished, prefix) {
+		var lastUrl = null;
+		c = new connector({
+			database: 'test'
+		})
+		c.connect();
+		c.loadDocument({keys: ['name', 'age'], type: 'not_a_person'}, function(error, doc) {
+			equal('warn', error.level, prefix + ' error object should not be null');
+			finished();
+		});
+	},
+	
+	'should create a map function with an if statement filtering by type': function(finished, prefix) {
+		var c = createMockConnection();
+		c.createView({
+			keys: ['name'],
+			type: 'person'
+		});
+		equal(true, lastDocument.views.by_name.map.indexOf('if (doc.type == \'person\') {') > 0, prefix + ' map function not properly created.');
+		finished();
+	},
+	
+	'should create a map function with a single value being emitted when a single value is passed in': function(finished, prefix) {
+		var c = createMockConnection();
+		c.createView({
+			keys: ['name'],
+			type: 'person'
+		})
+		equal(true, lastDocument.views.by_name.map.indexOf('emit(doc.name, doc);') > 0, prefix + ' map function not properly created.');
+
+		c.createView({
+			keys: 'name',
+			type: 'person'
+		})
+		equal(true, lastDocument.views.by_name.map.indexOf('emit(doc.name, doc);') > 0, prefix + ' map function not properly created.');
+		finished();
+	},
+	
+	'should create a map function with an array when multiple keys are provided': function(finished, prefix) {
+		var c = createMockConnection();
+		c.createView({
+			keys: ['name', 'age'],
+			type: 'person'
+		});
+		equal(true, lastDocument.views.by_name_age.map.indexOf('emit([doc.name, doc.age], doc);') > 0, prefix + ' map function not properly created.');
+		finished();
+	},
+	
+	'should create a map function with an array when multiple keys are provided': function(finished, prefix) {
+		var c = createMockConnection();
+		c.createView({
+			keys: ['name', 'age'],
+			type: 'person'
+		});
+		equal(true, lastDocument.views.by_name_age.map.indexOf('emit([doc.name, doc.age], doc);') > 0, prefix + ' map function not properly created.');
+		finished();
+	},
+	
+	'when two createView calls are executed in a row it should add both views to a design': function(finished, prefix) {
+		endtableEngine = new endtable.Engine({
+			database: 'test'
+		});
+
+		c = endtableEngine.connector
+		
+		function createTwoViews() {
+			c.createView({
+				keys: ['name', 'type', 'age'],
+				type: 'two_view_test'
+			});
+			
+			c.createView({
+				keys: ['type', 'name', 'age'],
+				type: 'two_view_test'
+			});
+		
+			var intervalId = setInterval(function() {
+				endtableEngine.loadDocument('_design/two_view_test', function(error, doc) {
+					if (typeof doc.views.by_name_type_age == 'object') {
+						if (typeof doc.views.by_type_name_age == 'object') {
+							clearInterval(intervalId);
+							finished();
+						}
+					};
+				});
+			}, 50);
+		}
+		
+		// Make sure the database is empty before creating the two views.
+		endtableEngine.loadDocument('_design/two_view_test', function(error, doc) {
+			if (doc) {
+				c.deleteDocument({_id: doc._id, _rev: doc._rev}, function(err, doc) {
+					createTwoViews();
+				});
+			} else {
+				createTwoViews();
+			}
+		});
+	}
+};
+
+
+lastDocument = {};
+function createMockConnection() {
+	var c = new connector({
+		database: 'test'
+	})
+	c.connection = {
+		saveDoc: function(documentName, document) {
+			lastDocument = document
+		},
+		getDoc: function(documentName, callback) {
+			callback(null, null);
+		},
+	}
+	c.synchronousProcessor = {
+		runSynchronous: function(context, callback, arguments) {
+			callback.apply(context, arguments);
+		}
+	}
+	return c;
+}
---
+++ tests/index.js
@@ -0,0 +1,68 @@
+var endtable = require('../lib'),
+	fs = require('fs'),
+	couchConnectorTests = require('./couch-connector-test'),
+	puts = require('sys').puts,
+	tests = [];
+	
+var engine = new endtable.Engine({
+	database: 'test',
+	host: 'localhost',
+	user: '',
+	password: ''
+});
+ 
+function run(callback, test) {
+	callback(
+		function() {
+			puts(test + ' \033[32m[Success]\033[m');
+			if (tests.length == 0) {
+			    puts(' \033[32mAll tests finished.\033[m');
+			    process.exit();
+			}
+			
+			var nextTest = tests.shift();
+			nextTest();
+		},
+		test + ': '
+	);
+}
+
+function addTests(testsObject) {
+	for (var test in testsObject) {
+		(function(func, name) {
+			tests.push(function() {
+				run(func, name);
+			});
+		})(testsObject[test], test);
+	}
+}
+
+(function loadFixtures() {
+	var fixtures = {
+		'people': ''
+	};
+	var size = 0;
+	var count = 0;
+	
+	for (var fixtureName in fixtures) {
+		if (fixtures.hasOwnProperty(fixtureName)) {
+			fixtures[fixtureName] = JSON.parse( fs.readFileSync('tests/fixtures/' + fixtureName + '.json') );
+			size += fixtures[fixtureName].length;
+		}
+	}
+
+	for (var fixtureName in fixtures) {
+		if (fixtures.hasOwnProperty(fixtureName)) {
+			for (var i = 0, fixture; (fixture = fixtures[fixtureName][i]) != null; i++) {
+				engine.saveDocument({type: fixture.type, fields: fixture}, function(error, doc) {
+					count++;
+					if (count == size) {
+						tests.shift()();
+					}
+				});
+			}
+		}
+	}
+})();
+
+addTests(couchConnectorTests.tests);
